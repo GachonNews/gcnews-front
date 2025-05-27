b895f9fb1818ffd6f0508c43b4c644d13501f32e
ImHarim <jjh1234y@gachon.ac.kr>
2025-05-27 23:00:35 +0900

    api connection complete?

--- gcnews/src/App.jsx
+++ gcnews/src/App.jsx
@@ -9,12 +9,8 @@ import Layout from "./components/Layout";
 import MainLayout from "./layouts/MainLayout";
 import MyPageLayout from "./layouts/MyPageLayout";
 import NewsPage from "./pages/NewsPage";
-<<<<<<< HEAD
 import CalendarPage from './pages/CalendarPage';  // CalendarPage import 추가
 import CalendarLayout from './layouts/CalendarLayout'; // 새로운 레이아웃 import
-=======
-import CalendarPage from "./pages/CalendarPage"; // CalendarPage import 추가
->>>>>>> 80a5f2b11ab1b751788e62ad4499435faa7af000
 
 export default function App() {
   return (
@@ -27,15 +23,10 @@ export default function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/signup-complete" element={<SignupComplete />} />
         </Route>
-<<<<<<< HEAD
-
-=======
->>>>>>> 80a5f2b11ab1b751788e62ad4499435faa7af000
         {/* 메인 레이아웃 (뉴스 관련 페이지) */}
         <Route element={<MainLayout />}>
           <Route path="/news" element={<NewsPage />} />
           <Route path="/news/:category" element={<NewsPage />} />
-<<<<<<< HEAD
           
         </Route>
 
@@ -49,17 +40,6 @@ export default function App() {
         <Route element={<CalendarLayout />}>
           <Route path="/calendar" element={<CalendarPage />} />
         </Route>
-=======
-        </Route>
-        {/* 마이페이지 레이아웃 (마이페이지, 프로필 수정 등) */}
-        <Route element={<MyPageLayout />}>
-          <Route path="/mypage" element={<MyPage />} />
-          <Route path="/edit" element={<EditProfile />} />
-        </Route>
-        {/* 캘린더 페이지 */}
-        <Route path="/calendar" element={<CalendarPage />} />{" "}
-        {/* /calendar 경로로 CalendarPage 추가 */}
->>>>>>> 80a5f2b11ab1b751788e62ad4499435faa7af000
       </Routes>
     </BrowserRouter>
   );
